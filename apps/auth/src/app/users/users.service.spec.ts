8b4b7dcd8f5d6d3341a7fc696d90a3da8842b34f
Karol Kluba <karol@mysmarthotel.com>
2025-04-16 20:25:29 +0200

    refactor: mock prisma service in spec files

--- apps/auth/src/app/users/users.service.spec.ts
+++ apps/auth/src/app/users/users.service.spec.ts
@@ -1,14 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
-import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [PrismaModule],
-      providers: [UsersService],
+      providers: [UsersService, { provide: PrismaService, useValue: {} }],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
