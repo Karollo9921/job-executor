761a1cf8fa147b15803c30a2a2866a70a54e54ab
Karol Kluba <karollo9921@gmail.com>
2025-05-03 21:19:26 +0200

    feat: add script to load testing

---
+++ scripts/fibonacci.mjs
@@ -0,0 +1,77 @@
+const AUTH_API_URL = 'http://localhost:30800/graphql';
+
+const LOGIN_MUTATION = `
+  mutation Login($loginInput: LoginInput!) {
+    login(input: $loginInput) {
+      id
+    }
+  }
+`;
+
+const JOBS_API_URL = 'http://localhost:3001/graphql';
+
+const EXECUTE_JOB_MUTATION = `
+  mutation ExecuteJob(executeJobInput: ExecuteJobInput!) {
+    executeJob(executeJobInput: $executeJobInput) {
+      name
+    }
+  }
+`;
+
+async function login(email, password) {
+  const response = await fetch(AUTH_API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: LOGIN_MUTATION,
+      variables: {
+        loginInput: { email, password },
+      },
+    }),
+  });
+
+  const data = await response.json();
+  const cookies = response.headers.get('set-cookie');
+
+  return { data, cookies };
+}
+
+async function executeJobWithInput(executeJobInput, cookies) {
+  const response = await fetch(JOBS_API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Cookie: cookies,
+    },
+    body: JSON.stringify({
+      query: EXECUTE_JOB_MUTATION,
+      variables: {
+        loginInput: { executeJobInput },
+      },
+    }),
+  });
+
+  const data = await response.json();
+
+  return { data };
+}
+
+(async () => {
+  const { data: loginData, cookies } = await login('karol.kluba89@gmail.com', 'Password123!');
+  if (loginData.data.login.id) {
+    const n = 10;
+    console.log(`Executing Fibonacci with n = ${n}`);
+    const executeJobInput = {
+      name: 'Fibonacci',
+      data: Array.from({ length: n }, () => ({
+        iteration: Math.floor(Math.random() * 5000) + 1,
+      })),
+    };
+    const data = await executeJobWithInput(executeJobInput, cookies);
+    console.log(data);
+  } else {
+    console.error('Login failed');
+  }
+})();
